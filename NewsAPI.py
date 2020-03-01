f25e6b6cc941b5e26bff68c523461b8e5e7209d4
cathychen <chencath@hu-berlin.de>
2020-03-01 18:42:56 +0000

    Give subscriptions to the build-in methods

--- NewsAPI.py
+++ NewsAPI.py
@@ -12,12 +12,12 @@ class NewsApi:
     """Class for accessing StockTwits API functions.
 
   Methods:
-    GetCategories  -- Download categories for detailed retrieval information.
-    GetCompany -- Search for users, symbols, or both.
-    GetStory -- Download the stories about human rights.
+    GetResource  -- get the resources of news.
+    GetHeadlines -- Download top headlines of specific country.
+    GetEverything -- Download top headlines of specific country.
 
   For more detail on HumanRight API, see the documentation at
-  https://www.business-humanrights.org/en/using-our-business-and-human-rights-data-to-bring-about-change
+  https://newsapi.org/
   """
 
     def __init__(self, key: str):
@@ -65,60 +65,5 @@ class NewsApi:
             print("HTTP Request fail {}\r\n{}".format(fullUri, e))
             return None
         return json.loads(result.content)
-
-
-
-    def GetCompany(self, id, languageCode = "en"):
-
-        fullUri = ('{baseUri}companies/{id}'.format(baseUri=self.baseUri, id=id))
-        getParams = { 'langcode': languageCode }
-
-        try:
-            result = requests.get(fullUri, headers=self.headers, params=getParams)
-        except Exception as e:
-            print("HTTP Request fail {}\r\n{}".format(url, e))
-            return None
-        return json.loads(result.content)
-
-    # def GetCompanies(self, page, responseRate, responseRateOperator: ResponseRateOperators, languageCode ="en", ):
-    #     fullUri = ('{baseUri}companies'.format(baseUri=self.baseUri))
-    #     getParams = {
-    #                     'page': page,
-    #                     'langcode': languageCode,
-    #                     'response_rate': responseRate,
-    #                     'response_rate_operator': responseRateOperator.value
-    #                 }
-    #     try:
-    #         result = requests.get(fullUri, headers=self.headers, params=getParams)
-    #     except Exception as e:
-    #         print("HTTP Request fail {}\r\n{}".format(url, e))
-    #         return None
-    #     return json.loads(result.content)
-    #
-    # def GetStory(self, id, languageCode = "en"):
-    #
-    #     fullUri = ('{baseUri}stories/{id}'.format(baseUri=self.baseUri, id=id))
-    #     getParams = { 'langcode': languageCode }
-    #
-    #     try:
-    #         result = requests.get(fullUri, headers=self.headers, params=getParams)
-    #     except Exception as e:
-    #         print("HTTP Request fail {}\r\n{}".format(url, e))
-    #         return None
-    #     return json.loads(result.content)
-    #
-    # def GetStories(self, categoryId, languageCode = "en", fullDetails = 1):
-    #     fullUri = ('{baseUri}stories'.format(baseUri=self.baseUri))
-    #     getParams = {
-    #                     'langcode': languageCode,
-    #                     'categories': categoryId,
-    #                     'full_details': fullDetails
-    #                 }
-    #     try:
-    #         result = requests.get(fullUri, headers=self.headers, params=getParams)
-    #     except Exception as e:
-    #         print("HTTP Request fail {}\r\n{}".format(url, e))
-    #         return None
-    #     return json.loads(result.content)
 
 
