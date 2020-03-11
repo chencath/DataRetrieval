2f1de0f34cecb8a08d5de5da7c69853ce8f41f49
cathychen <chencath@hu-berlin.de>
2020-03-11 18:27:23 +0000

    Code developed for recursively retrieve messages to comply with the rate limit

--- Main_StockTwits_Coins.py
+++ Main_StockTwits_Coins.py
@@ -1,91 +1,83 @@
 
+
+""" The Web API provided by StockTwits https://api.stocktwits.com/developers/docs/api#streams-symbol-docs
+Retrieve symbol-related messages recursively,
+to comply with  rate limit, program needs to sleep until it get accessibility again
+
+"""
 import importlib
 import StockTwitsAPI
 importlib.reload(StockTwitsAPI)
-import pandas as pd
 import json
-import os
 import datetime as dt
 import time
+import pandas as pd
 from StockTwitsAPI import StockTwitsApi    # create module NewsAPI for object class "NewsApi"
-
-with open("access_token_stockTwits.txt", "r") as f:
-    access_token = f.read()[:-1]
-
-api = StockTwitsApi(access_token)
+import preprocessing as pre             # create preprocessing class for NLP
+importlib.reload(pre)
 
 
-def list_currencies():
-    """Return the list of currencies in database."""
-    with open("currencies.json", "r", encoding="utf-8") as f:
-        return (json.load(f))
 
-
-def read_stream(cur):
-    """Return the stream of messages in database related to cur.
-
-      cur must be a dictionary with at least keys "title" and "symbol".
-    An empty list is returned if the corresponding JSON file doesn't exist yet.
-      """
-    try:
-        with open("stream{}.json".format(cur["symbol"]), "r", encoding="utf-8") as f:
-            stream = json.load(f)
-        return (stream)
-    except FileNotFoundError:
-        print("Stream not found for {} ({}).".format(cur["title"], cur["symbol"]))
-        return ([])
-
-
-def collect(cur):
-  """Download cur-related messages that are older than the oldest in database.
+def read_stream(symbol):
+  """Return the stream of messages for symbol.
+  An empty list is returned if the corresponding JSON file doesn't exist yet.
 	"""
-  stream = read_stream(cur)
-  max = None if len(stream) == 0 else stream[-1]["id"]
-  more, success = True, True
-  while more:
-    kwargs = {"id": cur["id"]}
-    if max is not None:
-      kwargs["max"] = max
-    newstream = api.stream_symbol(**kwargs).json()
-    status = newstream["response"]["status"]
-    if status == 200: # Success
-      if len(newstream["messages"]) > 0:
-        stream += newstream["messages"]
-        max = stream[-1]["id"]
-      else:
-        more = False
-    elif status == 404: # Symbol not found
-      print(newstream["errors"][0]["message"])
-      more = False
-    else: # Other errors from StockTwits
-      [print(e["message"]) for e in newstream["errors"]]
-      success, more = False, False
-  if len(stream) > 0:
-    print("Oldest message collected was created on {}.".format(stream[-1]["created_at"]))
-    with open("stream{}.json".format(cur["symbol"]), "w", encoding="utf-8") as f:
-      json.dump(stream, f)
-  return(success)
-
-def update_database():
-    """Update database.
+  try:
+    with open("stream{}.json".format(symbol), "r", encoding="utf-8") as f:
+      stream = json.load(f)
+    return(stream)
+  except FileNotFoundError:
+    print("Stream not found for {}.".format(symbol))
+    return([])
 
 
-    """
+def collect(symbol, access_token):
+    """Download symbol-related messages recursively.
+    Due to rate limit, program needs to sleep until it get accessibility again
+      """
+    stream = read_stream(symbol)
+    api = StockTwitsApi(access_token)
+    more, success = True, True
+    while more:
+        newstream = api.stream_symbol(symbol).json()
+        status = newstream["response"]["status"]
+        if status == 200:  # Success
+            if len(newstream["messages"]) > 0:
+                stream = newstream["messages"] + stream
+                # since = stream[0]["id"]
+            else:
+                more = False
+        elif status == 404:  # Symbol not found
+            print(newstream["errors"][0]["message"])
+            more = False
+        else:  # Other errors from StockTwits
+            [print(e["message"]) for e in newstream["errors"]]
+            success, more = False, False
+    if len(stream) > 0:
+        print("Most recent message collected was created on {}.".format(stream[0]["created_at"]))
+        with open("stream{}.json".format(symbol), "w", encoding="utf-8") as f:
+            json.dump(stream, f)
+    return (success)
 
+def main():
+
+    with open("access_token_stockTwits.txt", "r") as f:
+        access_token = f.read()[:-1]
     start = dt.datetime.now()
-    currencies = list_currencies()
-    for cur in currencies:
-        print("Updating {}...".format(cur["title"]))
-        if read_stream(cur) == []:
-            callback = collect(cur)
-        while not (callback(cur)):
-            # 200 unregistered requests and 400 registered requests are allowed
-            # per hour.
-            start += dt.timedelta(hours=1)
-            print("Sleeping until {}...".format(start))
-            time.sleep((start - dt.datetime.now()).seconds)
-    # print("Making timeline...")
-    # pre.make_timeline()
+    Symbol='IBB'
+    with open("stream{}.json".format(Symbol), "w", encoding="utf-8") as f:
+        json.dump([], f)
+    while not (collect(symbol=Symbol, access_token=access_token) ):
+        start += dt.timedelta(hours=1)
+        print("Sleeping until {}...".format(start))
+        time.sleep((start - dt.datetime.now()).seconds)
 
 
-update_database()
+main()
+
+
+
+
+
+
+
