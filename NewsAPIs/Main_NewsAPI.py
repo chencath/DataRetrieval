fa21abe14dd0740001ffe722b445822f073241ba
chencath <chencath@hu-berlin.de>
2020-03-27 23:13:50 +0800

    1. Create NewAPIs folder and move newsApi related files to the folder. 2. Update main() so that we can get multiple days of news by given startdate and duration

--- NewsAPIs/Main_NewsAPI.py
+++ NewsAPIs/Main_NewsAPI.py
@@ -16,43 +16,35 @@ def CreateDF(JsonArray,columns):
             itemStruct[cunColumn] = item[cunColumn]
 
         dfData = dfData.append(itemStruct,ignore_index=True)
-    #dfData = dfData.append({'id': item['id'], 'name': item['name'], 'description': item['description']},
-    #                               ignore_index=True)
-
     return dfData
 
 def main():
     # access_token_NewsAPI.txt must contain your personal access token
-    #with open("access_token_NewsAPI.txt", "r") as f:
-    #    myKey = f.read()[:-1]
-    # myKey = '08cf4b67d84a47b4b3fc72d7bab9f996'
-    myKey = 'b0df47573c1d41c0a6ebefe348693b60'
-    api = NewsApi(myKey)
+    with open("access_token_NewsAPI.txt", "r") as f:
+        myKey = f.read()[:-1]
 
-    # get  news for specific symbol (Way 1)
+    # Parameters for query
     symbol = "coronavirus"
-    sources = ''
-    columns = ['author', 'publishedAt', 'title', 'description', 'content', 'source']
-    limit = 500     # maximum requests per day
-
+    sources = 'bbc.co.uk'
     pageSize = 100
-    queryDate = dt.datetime(2020,3,26)
+    startDateTime = dt.datetime(2020, 3, 1)
+    durationDays = 10
 
-    startDateTime = queryDate
-    endDateTime = queryDate + dt.timedelta(days=1)
+    # end of parameters for query section
 
+    api = NewsApi(myKey)
+    endDateTime = startDateTime + dt.timedelta(days=durationDays) - dt.timedelta(seconds=1)
     columns = ['author', 'publishedAt', 'title', 'description', 'content', 'source']
     df = pd.DataFrame({'author': [], 'publishedAt': [], 'title': [], 'description': [], 'content': [], 'source': []})
 
     while True:
         result = api.GetEverything(symbol, startDateTime, endDateTime, 'en', sources, pageSize, 'publishedAt')
         numOfArticles = len(result['articles'])
-        print(numOfArticles)
+        print("got " + str(numOfArticles) + " articles")
         if numOfArticles == 0:
             break
 
         endDateTime = dt.datetime.strptime(result['articles'][numOfArticles - 1]['publishedAt'], "%Y-%m-%dT%H:%M:%SZ") - dt.timedelta(seconds=1)
-        print(result)
         rst = CreateDF(result['articles'], columns)
         df = df.append(rst, ignore_index=True)
 
