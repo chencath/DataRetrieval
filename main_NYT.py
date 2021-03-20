7fa96d0f3c888a061c9d966c5462275db0b0ae07
cathychen <chencath@hu-berlin.de>
2021-03-20 19:14:32 +0000

    refine the codes

--- main_NYT.py
+++ main_NYT.py
@@ -5,23 +5,21 @@ import os
 import glob
 import importlib
 import NewYorkTime.utils
+import pandas as pd
 
 importlib.reload(NewYorkTime.api_NYT)
 
 """  Step 1: API requests and data retrieval    """
 
 # load API key, which can be generated for free here: https://developer.nytimes.com/accounts/create
-#os.chdir('/Users/cathychen/PycharmProjects/resources')
+os.chdir('/Users/cathychen/PycharmProjects/resources')
 
-#with open("access_token_NYT.txt", "r") as keyfile:
-#    api_key = keyfile.readlines()[0]
-api_key = 'cA4fWcbrjd8y80iYfYb7o6YjRksa0zl1'
+with open("access_token_NYT.txt", "r") as keyfile:
+   api_key = keyfile.readlines()[0]
 
-#wk_dir='/Users/cathychen/PycharmProjects/resources/NYT_archive'
-wk_dir = 'e:\\temp'
+wk_dir='/Users/cathychen/PycharmProjects/resources/NYT_archive'
 # access api of new york time news, given api_key and working_dir
-
-#NewYorkTime.api_NYT.ApiRetrival(api_key, startYear=2019, endYear=2020, working_dir=wk_dir)
+NewYorkTime.api_NYT.ApiRetrival(api_key, startYear=2019, endYear=2020, working_dir=wk_dir)
 
 """  Step 2: Parse JSON files to dataframe   """
 from NewYorkTime.ParseJSON import PasringJSON
@@ -35,3 +33,4 @@ for fileName in fileNames:
     result.append(NewYorkTime.utils.parse_monthly_json(fileName))
 
 print(result)
+results = pd.concat(result)
