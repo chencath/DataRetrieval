c3619b3628bbe49b557139dcc3954ed6d518063d
cathychen <chencath@hu-berlin.de>
2021-03-20 22:56:45 +0000

    refine the codes

--- main_NYT.py
+++ main_NYT.py
@@ -6,6 +6,7 @@ import glob
 import importlib
 import NewYorkTime.utils
 import pandas as pd
+import nltk
 
 importlib.reload(NewYorkTime.api_NYT)
 
@@ -33,4 +34,40 @@ for fileName in fileNames:
     result.append(NewYorkTime.utils.parse_monthly_json(fileName))
 
 print(result)
-results = pd.concat(result)
+# combine monthly dataframes, sort articles by publication date, and reset index
+df = pd.concat(result)
+df.sort_values(by="pub_date", inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+"""  Step 3: Slice dataframe   """
+# select only certain sections (1.4m out of total of around 3.9m articles)
+# list of all sections/desks here: https://developer.nytimes.com/docs/articlesearch-product/1/overview
+sections = ["World", "U.S.", "Business Day", "Technology"]
+desks = ["Business/Financial Desk", "Business", "Financial Desk"]
+print(df.columns)
+df = df[df.section_name.isin(sections) | df.news_desk.isin(desks)]
+
+# get rid of weird length abstracts
+# 32k articles removed that were outside of +-1 magnitude from median length of ~200 chars/~2-3 sentences
+df = df[(df.abstract.str.len() >=20) & (df.abstract.str.len() <2000)]
+
+"""  Step 4: Basic NLP   """
+# remove non-ascii characters
+# source: https://stackoverflow.com/questions/36340627/remove-non-ascii-characters-from-pandas-column
+df.abstract.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
+
+# remove special characters
+df.abstract.replace({r'[^a-zA-z\s]':''}, regex=True, inplace=True)
+
+# remove underscores
+df.abstract.replace({r'_':''}, regex=True, inplace=True)
+
+# remove start and end white spaces and make lower case
+df.abstract = df.abstract.str.strip().str.lower()
+
+# exclude standard nltk stopwords + defined stopwords + single letters
+stop = nltk.corpus.stopwords.words("english")
+print(stop)
+df['abstract'] = df['abstract'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+df.to_csv('NYT_DF.csv')
+
