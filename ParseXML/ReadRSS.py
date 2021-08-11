be5b7b84b5c2228833ee5b17ba36487e77d638ff
cathychen <chencath@hu-berlin.de>
2021-08-11 10:18:04 +0100

    add some notes

--- ParseXML/ReadRSS.py
+++ ParseXML/ReadRSS.py
@@ -14,7 +14,7 @@ import pandas as pd
 import os
 
 
-# retrieve RSS feedback
+# retrieve RSS news feed from Financial Times
 content = feedparser.parse("https://www.ft.com/?edition=international&format=rss")
 
 # list all titles
